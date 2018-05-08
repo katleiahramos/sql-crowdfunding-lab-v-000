5c1185062926508dc7d913a2c453bf70bbb28ab6
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-08 19:32:27 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,7 @@ FROM users
 INNER JOIN pledges
 ON pledges.user_id = users.id
 GROUP BY name
-ORDER BY SUM(pledges.amount)"
+ORDER BY SUM(pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
