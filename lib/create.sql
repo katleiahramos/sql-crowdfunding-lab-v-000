1935cb9dacd20642e6f9e2dd9761693a98bfda3b
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-08 18:39:40 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,7 +1,23 @@
 CREATE TABLE projects(
   id INTEGER PRIMARY KEY,
   category TEXT,
-  funding_goal INTEGER, 
-  start_date INTEGER, 
+  funding_goal INTEGER,
+  start_date INTEGER,
   end_date INTEGER
+);
+
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY, 
+  name TEXT, 
+  age INTEGER
+  
+);
+
+
+CREATE TABLE pledges(
+  id INTEGER PRIMARY KEY, 
+  amount INTEGER, 
+  user_id INTEGER, 
+  project_id INTEGER
+  
 );
