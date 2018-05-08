c8007401d2cd35701198a4e565a94cc4705d0a70
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-08 19:27:41 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,10 @@ GROUP BY users.name
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-""
+"SELECT title
+FROM projects
+INNER JOIN pledges
+ON pledges.project_id = projects.id"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
