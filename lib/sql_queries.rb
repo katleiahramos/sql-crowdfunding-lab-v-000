5431e4f38c98b065b29988893b99ae39515dafda
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-08 19:27:52 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,9 +24,9 @@ GROUP BY users.name
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT title 
-FROM projects 
-INNER JOIN pledges 
+"SELECT title
+FROM projects
+INNER JOIN pledges
 ON pledges.project_id = projects.id
 GROUP BY title; "
 end
